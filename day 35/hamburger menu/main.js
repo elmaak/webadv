71474b47766f6f81b7acdc582a92b7301cfde6de
elmaak <elmakutllovci0@gmail.com>
2025-10-04 13:48:31 +0100

    .

--- day 35/hamburger menu/main.js
+++ day 35/hamburger menu/main.js
@@ -1,25 +1,23 @@
-function openMenu(){
-    var menu=document.getElementById("links");
-    if(menu.style.display === "block"){
-        menu.style.display = "none";
-    }else{
-        menu.style.display = "block";
-    }
+function openMenu() {
+  var menu = document.getElementById("myLinks");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 }
-
-function search(){
-    var input, fliter, ul, li, a, i, txtvalue;
-    input=document.getElementById("myInput");
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     ul = document.getElementById("myList");
-    li=ul.getElementByTagname("li");
-
-    for(i=0; i<li.length; i++){
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
         a = li[i];
-        txtvalue = a.textContent || a.innerText;
-        if(txtvalue.toUpperCase().indexOf(fliter)>-1){
+        txtValue = a.textContent;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "block";
-        }else{
+        } else {
             li[i].style.display = "none";
         }
     }
